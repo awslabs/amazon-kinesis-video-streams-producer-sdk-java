43b9bd315d1cc36b207920887952718367bdd10b
sirknightj <jggunawa@amazon.com>
2025-05-16 09:19:40 -0700

    Delete the extra stuff, adjust the CI to use the local JNI, CMake enhancements like adding clean-all target and adjusting the Java not found error message

--- CMake/Utilities.cmake
+++ CMake/Utilities.cmake
@@ -1,8 +1,7 @@
 # only fetch target repo for add_subdirectory later
 function(fetch_repo lib_name)
   set(supported_libs
-      kvspic
-      kvscproducer)
+      kvspic)
   list(FIND supported_libs ${lib_name} index)
   if(${index} EQUAL -1)
     message(WARNING "${lib_name} is not supported for fetch_repo")
@@ -34,81 +33,6 @@ function(fetch_repo lib_name)
   if(result)
     message(FATAL_ERROR "CMake step for lib${lib_name} failed: ${result}")
   endif()
-endfunction()
-
-# build library from source
-function(build_dependency lib_name)
-  set(supported_libs
-      autoconf
-      automake
-      kvspic
-      log4cplus)
-  list(FIND supported_libs ${lib_name} index)
-  if(${index} EQUAL -1)
-    message(WARNING "${lib_name} is not supported to build from source")
-    return()
-  endif()
-
-  set(target_found NOTFOUND)
-
-  if (${lib_name} STREQUAL "autoconf" OR ${lib_name} STREQUAL "automake")
-    find_program(
-      target_found
-      NAMES ${lib_name}
-      PATHS ${OPEN_SRC_INSTALL_PREFIX}/bin
-      NO_DEFAULT_PATH)
-  else()
-    set(lib_file_name ${lib_name})
-    if (${lib_name} STREQUAL "openssl")
-      set(lib_file_name ssl)
-    endif()
-
-    find_library(
-      target_found
-      NAMES ${lib_file_name}
-      PATHS ${OPEN_SRC_INSTALL_PREFIX}/lib
-      NO_DEFAULT_PATH)
-  endif()
-
-  if(target_found)
-    message(STATUS "${lib_name} already built")
-    return()
-  endif()
-
-  # anything after lib_name(${ARGN}) are assumed to be arguments passed over to
-  # library building cmake.
-  set(build_args ${ARGN})
-
-  file(REMOVE_RECURSE ${KINESIS_VIDEO_OPEN_SOURCE_SRC}/lib${lib_name})
-
-  if (WIN32 OR NOT PARALLEL_BUILD)
-    set(PARALLEL_BUILD "")  # No parallel build for Windows
-  else()
-    set(PARALLEL_BUILD "--parallel")  # Enable parallel builds for Unix-like systems
-  endif()
-
-  # build library
-  configure_file(
-    ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Dependencies/lib${lib_name}-CMakeLists.txt
-    ${KINESIS_VIDEO_OPEN_SOURCE_SRC}/lib${lib_name}/CMakeLists.txt COPYONLY)
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} ${build_args}
-            -DOPEN_SRC_INSTALL_PREFIX=${OPEN_SRC_INSTALL_PREFIX} -G
-            ${CMAKE_GENERATOR} .
-    RESULT_VARIABLE result
-    WORKING_DIRECTORY ${KINESIS_VIDEO_OPEN_SOURCE_SRC}/lib${lib_name})
-  if(result)
-    message(FATAL_ERROR "CMake step for lib${lib_name} failed: ${result}")
-  endif()
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} --build . ${PARALLEL_BUILD}
-    RESULT_VARIABLE result
-    WORKING_DIRECTORY ${KINESIS_VIDEO_OPEN_SOURCE_SRC}/lib${lib_name})
-  if(result)
-    message(FATAL_ERROR "CMake step for lib${lib_name} failed: ${result}")
-  endif()
-
-  file(REMOVE_RECURSE ${KINESIS_VIDEO_OPEN_SOURCE_SRC}/lib${lib_name})
 endfunction()
 
 function(enableSanitizer SANITIZER)
