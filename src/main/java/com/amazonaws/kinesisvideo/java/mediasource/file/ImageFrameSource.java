c1e199619be380e2254ae34b98e05cf8aea2f673
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2025-06-03 11:38:40 -0700

    Address comments

--- src/main/java/com/amazonaws/kinesisvideo/java/mediasource/file/ImageFrameSource.java
+++ src/main/java/com/amazonaws/kinesisvideo/java/mediasource/file/ImageFrameSource.java
@@ -15,9 +15,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.amazonaws.kinesisvideo.producer.FrameFlags.FRAME_FLAG_KEY_FRAME;
 import static com.amazonaws.kinesisvideo.producer.FrameFlags.FRAME_FLAG_NONE;
@@ -37,13 +39,13 @@ public class ImageFrameSource {
 
     private final int totalFiles;
     private OnStreamDataAvailable mkvDataAvailableCallback;
-    private boolean isRunning = false;
+    private final AtomicBoolean isRunning = new AtomicBoolean(false);
     private int frameCounter;
     private final Log log = LogFactory.getLog(ImageFrameSource.class);
     private final String metadataName = "ImageLoop";
     private int metadataCount = 0;
     private long currentFrameTimestampMs;
-    private long executorShutdownTimeoutSeconds = 5L;
+    private final long executorShutdownTimeoutSeconds = 5L;
 
     public ImageFrameSource(final ImageFileMediaSourceConfiguration configuration) {
         this.configuration = configuration;
@@ -58,16 +60,16 @@ public class ImageFrameSource {
     }
 
     public void start() {
-        if (isRunning) {
+        if (isRunning.get()) {
             throw new IllegalStateException("Frame source is already running");
         }
 
-        isRunning = true;
+        isRunning.set(true);
         startFrameGenerator();
     }
 
     public void stop() {
-        isRunning = false;
+        isRunning.set(false);
         stopFrameGenerator();
     }
 
@@ -92,7 +94,7 @@ public class ImageFrameSource {
         final double frameDurationMs = (double) Duration.ofSeconds(1L).toMillis() / fps;
         long nextFrameTimeNs = System.nanoTime(); // to prevent time drift
 
-        while (isRunning) {
+        while (isRunning.get()) {
             if (mkvDataAvailableCallback != null) {
                 mkvDataAvailableCallback.onFrameDataAvailable(createKinesisVideoFrameFromImage(frameCounter, currentFrameTimestampMs));
                 if (isMetadataReady()) {
@@ -153,10 +155,20 @@ public class ImageFrameSource {
         executor.shutdown();
         try {
             if (!executor.awaitTermination(this.executorShutdownTimeoutSeconds, TimeUnit.SECONDS)) {
-                executor.shutdownNow();
+                log.warn("Executor did not terminate in time. Forcing shutdown.");
+                final List<Runnable> droppedTasks = executor.shutdownNow();
+                log.warn("Number of dropped tasks: " + droppedTasks.size());
+                for (final Runnable task : droppedTasks) {
+                    log.warn("Dropped task of type: " + task.getClass().getName());
+                }
             }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
+        } catch (final InterruptedException e) {
+            log.error("Executor shutdown interrupted with Exception ", e);
+            final List<Runnable> droppedTasks = executor.shutdownNow();
+            log.warn("Number of dropped tasks: " + droppedTasks.size());
+            for (final Runnable task : droppedTasks) {
+                log.warn("Dropped task of type: " + task.getClass().getName());
+            }
             Thread.currentThread().interrupt();
         }
     }
