39b735b3d5e690db6d7bd12581bd0ba3dc1f1bf9
sirknightj <jggunawa@amazon.com>
2025-08-17 09:20:14 -0700

    Prevent NullPointerException in the OnStreamDataAvailable (#249)

--- src/main/java/com/amazonaws/kinesisvideo/internal/mediasource/DefaultOnStreamDataAvailable.java
+++ src/main/java/com/amazonaws/kinesisvideo/internal/mediasource/DefaultOnStreamDataAvailable.java
@@ -1,29 +1,43 @@
 package com.amazonaws.kinesisvideo.internal.mediasource;
 
 import com.amazonaws.kinesisvideo.common.exception.KinesisVideoException;
+import com.amazonaws.kinesisvideo.common.preconditions.Preconditions;
 import com.amazonaws.kinesisvideo.internal.client.mediasource.MediaSourceSink;
 import com.amazonaws.kinesisvideo.producer.KinesisVideoFrame;
 
+import javax.annotation.Nonnull;
+
+/**
+ * Forwards the received media data to the configured {@link MediaSourceSink}.
+ */
 public class DefaultOnStreamDataAvailable implements OnStreamDataAvailable {
+    @Nonnull
     final MediaSourceSink mediaSourceSink;
 
-    public DefaultOnStreamDataAvailable(final MediaSourceSink mediaSourceSink) {
+    @SuppressWarnings("ConstantConditions")
+    public DefaultOnStreamDataAvailable(@Nonnull final MediaSourceSink mediaSourceSink) {
+        Preconditions.checkArgument(mediaSourceSink != null, "MediaSourceSink cannot be null");
         this.mediaSourceSink = mediaSourceSink;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Override
-    public void onFrameDataAvailable(final KinesisVideoFrame frame) throws KinesisVideoException {
+    public void onFrameDataAvailable(@Nonnull final KinesisVideoFrame frame) throws KinesisVideoException {
+        Preconditions.checkArgument(frame != null, "KinesisVideoFrame cannot be null");
+
         // ignore frame of size 0
         if (frame.getSize() == 0) {
             throw new KinesisVideoException("Empty frame is provided in frame data available.");
         }
 
-        mediaSourceSink.onFrame(frame);
+        this.mediaSourceSink.onFrame(frame);
     }
 
     @Override
-    public void onFragmentMetadataAvailable(final String metadataName, final String metadataValue,
+    public void onFragmentMetadataAvailable(@Nonnull final String metadataName, @Nonnull final String metadataValue,
                                             final boolean persistent) throws KinesisVideoException {
-        mediaSourceSink.onFragmentMetadata(metadataName, metadataValue, persistent);
+        Preconditions.checkArgument(metadataName != null, "Metadata name cannot be null");
+        Preconditions.checkArgument(metadataValue != null, "Metadata value cannot be null");
+        this.mediaSourceSink.onFragmentMetadata(metadataName, metadataValue, persistent);
     }
 }
