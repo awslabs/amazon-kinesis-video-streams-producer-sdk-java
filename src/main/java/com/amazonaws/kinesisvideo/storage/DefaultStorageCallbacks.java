43cb0b11c526faa7352c1781d8e706795d26e565
sirknightj <jggunawa@amazon.com>
2025-08-16 20:26:13 -0700

    Add package-info for the storage package (#256)

--- src/main/java/com/amazonaws/kinesisvideo/storage/DefaultStorageCallbacks.java
+++ src/main/java/com/amazonaws/kinesisvideo/storage/DefaultStorageCallbacks.java
@@ -2,9 +2,29 @@ package com.amazonaws.kinesisvideo.storage;
 
 import com.amazonaws.kinesisvideo.producer.StorageCallbacks;
 
+/**
+ * No-op implementation of storage callbacks. Extending this class allows convenient
+ * implementation of certain callbacks while leaving the rest as no-op.
+ *
+ * <p>For example:</p>
+ *
+ * <pre>
+ *StorageCallbacks storageCallbacks =
+ *    new DefaultStorageCallbacks() {
+ *        &#64;Override
+ *        public void storageOverflowPressure(final long remainingSizeBytes) {
+ *            super.storageOverflowPressure(remainingSize);
+ *
+ *            final long remainingSizeInMB = remainingSizeBytes / 1024L / 1024L;
+ *
+ *            log.warn("The buffer is approaching capacity: {} MB remaining", remainingSizeInMB);
+ *        }
+ *    };
+ *</pre>
+ */
 public class DefaultStorageCallbacks implements StorageCallbacks {
     @Override
-    public void storageOverflowPressure(long remainingSize) {
-        // TODO: Add actual storage overflow callback later
+    public void storageOverflowPressure(final long remainingSizeBytes) {
+        // No-op
     }
 }
---
+++ src/main/java/com/amazonaws/kinesisvideo/storage/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Concrete implementations of {@link com.amazonaws.kinesisvideo.producer.StorageCallbacks}.
+ */
+package com.amazonaws.kinesisvideo.storage;
