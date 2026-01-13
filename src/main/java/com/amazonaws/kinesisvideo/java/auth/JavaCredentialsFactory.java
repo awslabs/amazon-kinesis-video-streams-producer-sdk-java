8a524f0f80bf0c20116b26737ae911a1c8d94ce6
sirknightj <jggunawa@amazon.com>
2026-01-13 10:55:21 -0800

    Add credentials provider documentation (#289)
    * Add credentials provider documentation
    
    * Adjust wording based on the feedback

--- src/main/java/com/amazonaws/kinesisvideo/java/auth/JavaCredentialsFactory.java
+++ src/main/java/com/amazonaws/kinesisvideo/java/auth/JavaCredentialsFactory.java
@@ -2,6 +2,7 @@ package com.amazonaws.kinesisvideo.java.auth;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSSessionCredentials;
+import com.amazonaws.kinesisvideo.auth.KinesisVideoCredentials;
 import com.amazonaws.kinesisvideo.auth.KinesisVideoCredentialsProvider;
 import com.amazonaws.kinesisvideo.common.preconditions.Preconditions;
 
@@ -10,9 +11,14 @@ import javax.annotation.concurrent.ThreadSafe;
 import java.time.Duration;
 
 /**
- * Turns an AWS credentials provider into KinesisVideoCredentialsProvider.
- * There are two types of KinesisVideoCredentialsProvider - one that provides temporary credentials (session token),
- * and non-temporary that just provides access key + secret key.
+ * Turns an AWS credentials provider (which vends AWS credentials) into {@link KinesisVideoCredentialsProvider} (which vends {@link KinesisVideoCredentials}).
+ * There are two types of KinesisVideoCredentialsProvider:
+ * <ul>
+ *     <li>One that provides temporary ephemeral credentials (session token)</li>
+ *     <li>And static that just provides access key + secret key (also known as long-lived credentials)</li>
+ * </ul>
+ *
+ * @see JavaCredentialsProviderImpl
  */
 @ThreadSafe
 public final class JavaCredentialsFactory {
@@ -32,7 +38,13 @@ public final class JavaCredentialsFactory {
      * @param awsCredentialsProvider             AWSCredentialsProvider
      * @param credentialsRefreshIntervalFallback If the AWSCredentialsProvider returns temporary credentials, how often
      *                                           those credentials should be refreshed. Can be less than its actual expiration.
+     *                                           But <strong>should NOT be greater than the configured AWS expiration</strong> (e.g. role duration seconds).
+     *                                           <p>
+     *                                           If the AWSCredentialsProvider returns non-expiring credentials
+     *                                           (<strong>not recommended for production use</strong>),
+     *                                           use {@link KinesisVideoCredentials#getCredentialsNeverExpire()}.
      * @return the correct (temporary/non-temporary) Kinesis Video Credentials Provider based on the AWS Credentials Provider
+     * @see <a href="https://docs.aws.amazon.com/sdkref/latest/guide/access-users.html">Using AWS access keys to authenticate AWS SDKs and tools</a>
      */
     @SuppressWarnings({"ConstantConditions"}) // @Nonnull is a compile-time check, it can still be null at runtime
     public static KinesisVideoCredentialsProvider createKinesisVideoCredentialsProvider(@Nonnull final AWSCredentialsProvider awsCredentialsProvider,
